c8e15976b669f7e63d6b06cee92fd126e7e7fce6
Vinicius Montes <viniciusmontes@outlook.com.br>
2024-01-05 12:06:17 -0300

    Conditional styling

--- frontweb/src/pages/Home/Login/index.tsx
+++ frontweb/src/pages/Home/Login/index.tsx
@@ -59,7 +59,7 @@ const Login = () => {
               },
             })}
             type="text"
-            className="form-control base-input"
+            className={`form-control base-input ${errors.username ? 'is-invalid' : '' }`}
             placeholder="Email"
             name="username"
           />
@@ -71,7 +71,7 @@ const Login = () => {
           <input
             {...register('password', { required: 'Campo obrigatório' })}
             type="password"
-            className="form-control base-input "
+            className={`form-control base-input ${errors.password ? 'is-invalid' : '' }`}
             placeholder="Password"
             name="password"
           />
